41fd38afdd3e0934006b324321f27b7b892d9f8f
Abdullah Jimoh <jimoh.abdullah69@gmail.com>
2020-11-15 16:44:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+// // const generateB = document.querySelector('.b-quotes');
+// const text = document.querySelector('.Q');
+// // const textB = document.querySelector('.B');
+
+// // console.log(textB);
+
+
+
+// // const blacklistQuotes = [
+// //     `Wisdom is learning the boundaries of one’s designated lane`,
+// //     `Revenge isn’t a passion. It’s a disease. It eats at your mind and poisons your soul`,
+// //     `There’s a very fine line between love and hate.`,
+// //     `As my grandmother said, it’s good to have friends in low places.`,
+// //     `If you want your enemy to become your friend, create a problem for them and then solve it.`,
+// //     `You can’t do every silly thing you want to in life. You have to make choices and be happy with them.`,
+// //     `I had bullets, he had words. But when he was done talking for the first
+// //      time I truly understood which of those was more powerful.`,
+// //     `When you love someone, you have no control. That's what love is. Being Powerless`,
+// //     `In this world, there are no sides, only players`,
+// //     `Life is far too important a thing ever to talk seriously about`,
+// //     `The key to your sucess is a clear and consistent understanding of your own limitations`
+// // ]
+
+
+
+// function getGotQuotes() {
+    
+
+//     let result = Math.floor(Math.random() * gotQuotes.length);
+//     let randomRes = gotQuotes[result];
+        
+    
+//     text.textContent = randomRes;
+// }
+
+// // function getbBacklistQuotes() {
+    
+// //     let blacklistQ = Math.floor(Math.random() * blacklistQuotes.length);
+// //     let resultB = blacklistQuotes[blacklistQ];
+// //     // console.log(resultB);
+
+// //     textB.textContent = resultB;
+// // }
+
+// generate.addEventListener('click', getGotQuotes);
+// // generateB.addEventListener('click', getbBacklistQuotes);
+
+
